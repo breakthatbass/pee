e7352e433b46eb0aa4d884f9fb36cb6c6e4524d2
breakthatbass <gamache.taylor@gmail.com>
2020-11-24 14:03:39 -0500

    update

--- pee/__init__.py
+++ pee/__init__.py
@@ -5,5 +5,5 @@ pee: (p)ython fr(ee): Linux free command but for Mac Os built in Python.
     Display memory system info
 '''
 
-__version__ = '0.0.1'
+__version__ = '0.1.1'
 __author__ = 'Taylor Gamache'
